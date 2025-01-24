2611d6ca50b48e9e1e5769f52bf02ac272b6db4d
sathvikparasa <sathvikparasa007@gmail.com>
2025-01-24 14:20:59 -0800

    Fix first name issues and rename csv file
    Fixed suffixes showing up as first name and renamed csv file to persons.csv from data.csv. Added comments to make code look nice.

--- project/make_csv.py
+++ project/make_csv.py
@@ -3,23 +3,39 @@ import random
 import uuid
 import csv
 
+# Use Faker to create fake data for names
 fake = faker.Faker()
 
+# Headers for csv file
 headers = ["Id, First_Name, Last_Name, Email, Salary"]
 
-with open('data.csv', 'w', newline='') as file:
+# Create data.csv file and write to it
+with open('persons.csv', 'w', newline='') as file:
     writer  = csv.writer(file)
     writer.writerow(headers)
 
     for i in range(100):
+        # Get unique uid with uuid library
         uid = str(uuid.uuid4())
+
+        # Get name from fake.name() and split to first and last
         name = fake.name().split()
-        
         first = name[0]
         last = name[1]
 
-        email = first[0].lower() + last.lower() + '@gmail.com'
-        salary = round(random.uniform(0, 1000000), 2)
+        # Make sure no suffixes are the first name
+        if first in ["Mrs.", "Dr.", "Mr.", "Ms.", "Lt.", "Sir"]:
+            first = name[1]
+            last = name[2]
 
+        # Email is the first letter of first name + last name + @gmail.com
+        email = first[0].lower() + last.lower() + '@gmail.com'
+        
+        # Salary is random float from $0 to $1,000,000
+        salary = f"{round(random.uniform(0, 1000000), 2):.2f}"
+        # I had to change salary itself to a string for 2 decimal rounding 
+        #   but it doesn't matter in a csv file
+
+        # Write row of data to csv file
         row = [uid, first, last, email, salary]
         writer.writerow(row)
